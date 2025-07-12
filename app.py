422719701e52a3e5c0340a2bb6f56a7608d46473
nafisaa2402 <putrinafisa2402@gmail.com>
2025-07-12 09:04:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:"divider="gray")
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 st.markdown("*Streamlit* is **really** ***cool***.")
+
 st.metric(label="Temperature", value="70 °F")
 
 import pandas as pd
